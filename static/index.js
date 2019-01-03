ad20fe8b91e8fc299d834490f0e433b2fcd01ee1
potykion <potykion@gmail.com>
2019-01-04 00:58:26 +0300

    Focus on last task in list; append new blank task on blank task changed;

--- static/index.js
+++ static/index.js
@@ -2,7 +2,32 @@ var app = new Vue({
     el: '#app',
     data: {
         tasks: [
-            {title: "Уборочка"}
+            { title: "Уборочка" },
+            { title: "Проездной" },
+            { title: "" }
         ]
+    },
+    methods: {
+        taskChanged: async function (event) {
+            this.appendBlank();
+        },
+        appendBlank: function () {
+            if (this.lastTask.title != "") {
+                this.tasks = [...this.tasks, { title: "" }];
+            }
+        }
+    },
+    computed: {
+        lastTask: function () {
+            return this.tasks[this.tasks.length - 1];
+        }
+    },
+    // focus on last created input in list workaround
+    directives: {
+        focus: {
+            inserted: function (el) {
+                el.focus();
+            }
+        }
     }
 });

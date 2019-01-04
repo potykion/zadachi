95d08d6fb4304eeba59fc89d5308f6ea73e7a9ed
potykion <potykion@gmail.com>
2019-01-04 18:52:29 +0300

    Update fonts

--- static/index.js
+++ static/index.js
@@ -4,6 +4,11 @@ var app = new Vue({
         tasks: [
             { title: "Уборочка" },
             { title: "Проездной" },
+            { title: "THIS IS VEEEEEEEEEEEEEEEEEEEEEEERY LONG TASK" },
+            { title: "resto: orders_lite totals = auto report" },
+            { title: "rbcn_legals: features discuss / complete migration" },
+            { title: "rbcn_legals: migrate legals from gae" },
+            { title: "rbcn_legals: frontend: legals list, contract creation" },
             { title: "" }
         ]
     },
